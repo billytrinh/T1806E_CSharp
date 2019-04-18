24b487e019198b93ba385d652323963d3e85fd45
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-04-18 20:04:24 +0700

    code session 1

--- T1806E_CSharp/T1806E_CSharp/Program.cs
+++ T1806E_CSharp/T1806E_CSharp/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 namespace T1806E_CSharp
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("nhap n=");
+            int n = Console.Read();
+
+            Console.WriteLine("nhap 1 chuoi:");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("nhap 1 so thuc:");
+            float f = float.Parse(Console.ReadLine());
+
+            int[] arr = { 1, 2, 3, 4 };
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("hello");
+
+            List<string> ls = new List<string>();
+            ls.Add("hello");
+            ls.Add("world");
+            ls.Add("welcome");
+            Console.WriteLine(ls.Count);
+            foreach(string x in ls)
+            {
+                Console.WriteLine(x);
+            }
+
         }
     }
 }
