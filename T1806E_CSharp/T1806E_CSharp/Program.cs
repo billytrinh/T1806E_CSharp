9626c7647d49692bffa5a3eeda528dc8e3d941cb
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-04-18 19:33:43 +0700

    up program.cs

---
+++ T1806E_CSharp/T1806E_CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T1806E_CSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
