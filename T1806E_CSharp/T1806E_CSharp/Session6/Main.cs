79fe174d8855f5b281d41b57038e6923d9efd418
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-05-18 20:30:24 +0700

    * Main.cs: session 7
    * Program.cs:

--- T1806E_CSharp/T1806E_CSharp/Session6/Main.cs
+++ T1806E_CSharp/T1806E_CSharp/Session6/Main.cs
@@ -1,7 +1,44 @@
-﻿namespace T1806E_CSharp.Session63
+﻿using System;
+namespace DelegateDemo
 {
-    public class MainClass
+    public delegate void MyEventHandler(string msg);
+    public class Window
     {
+        public event MyEventHandler Click;
 
+        // mô phỏng vẽ cửa sổ
+
+        public void FireEvent()
+        {
+            if (Click != null)
+                Click("Event Fire.");
+        }
+
+    }
+
+    public class Tester
+    {
+        public static void Main()
+        {
+            //Window w = new Window();
+            //w.Click += w_Click;
+            //w.FireEvent(); // phát sinh sự kiện
+            //Console.ReadLine();
+            MyEventHandler meh = new MyEventHandler(w_Click);
+            meh += new MyEventHandler(e_Click);
+            meh("hahaha");
+
+
+        }
+
+        static void w_Click(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void e_Click(string msg)
+        {
+            Console.WriteLine(msg);
+        }
     }
 }
---
+++ T1806E_CSharp/T1806E_CSharp/Session7/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+namespace T1806E_CSharp.Session7
+{
+    public delegate void showString(string s);
+
+    public delegate void Numeric(int a, int b);
+
+    public class EventTest
+    {
+        public int age =0;
+        public event showString Click;
+
+        public void ShowEvent()
+        {
+            if(Click != null)
+            {
+                Click("Click here...");
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {   
+                if(this.age !=0 && this.age != value)
+                {
+                    Click("Age was changed");
+                }
+                this.age = value;
+            }
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EventTest ev = new EventTest();
+            ev.Click += showStringProgram;
+
+            ev.Age = 20;
+            ev.Age = 22;
+            //ev.ShowEvent();
+
+            showString ss = new showString(showStringProgram);
+            showString s2 = new showString(showStringPro);
+            ss += s2;
+            // Cach 1
+            ss("hello world");
+            // Cach 2
+            //ss.Invoke("world hello");
+
+            Numeric n1 = new Numeric(Sum);
+            Numeric n2 = new Numeric(Minus);
+            Numeric n3 = new Numeric(Multiple);
+            Numeric n4 = new Numeric(Divide);
+
+            //n1(3, 2);
+
+            //n1 += n3;
+
+            //n1(5, 6);
+
+            //n3 += n4;
+            //n2 += n3;
+            //n1 += n2;
+
+            n1 += n2;
+            n1 += n3;
+            n1 += n4;
+
+            n1(10, 5);
+        }
+
+        public static void showStringProgram(string x)
+        {
+            Console.WriteLine(x);
+        }
+
+        public static void showStringPro(string x)
+        {
+            Console.WriteLine("show String: " + x);
+        }
+
+        public static void Sum(int a,int b)
+        {
+            Console.WriteLine("Sum: " + (a + b));
+        }
+
+        public static void Minus(int a, int b)
+        {
+            Console.WriteLine("Minus: " + (a - b));
+        }
+
+        public static void Multiple(int a, int b)
+        {
+            Console.WriteLine("Mutiple: " + (a * b));
+        }
+
+        public static void Divide(int a, int b)
+        {
+            Console.WriteLine("Divide: " + (a / b));
+        }
+    }
+}
