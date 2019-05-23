59e5c060a1f40d0cb3041bb8838b9a0d6656f77f
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-05-23 19:00:53 +0700

    thread update

--- T1806E_CSharp/T1806E_CSharp/Session9/ThreadDemo.cs
+++ T1806E_CSharp/T1806E_CSharp/Session9/ThreadDemo.cs
@@ -7,10 +7,17 @@ namespace T1806E_CSharp.Session9
     public class ThreadDemo
     {
         public static void Main(string[] args)
-        {
+        {   
             RunOnThread("Main");
             Thread t = new Thread(RunOnThread);
             t.Start("Thread 2");
+
+            Thread t2 = new Thread(new ThreadDemo().RunRun);
+            t2.Start();
+
+            ThreadDemo td = new ThreadDemo();
+            Thread t3 = new Thread(td.RunRun);
+            t3.Start();
         }
 
         public static void RunOnThread(Object a)
@@ -20,6 +27,14 @@ namespace T1806E_CSharp.Session9
             {
                 Console.WriteLine(s +"--"+ i);
                 Thread.Sleep(300);
+            }
+        }
+        public void RunRun()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Runrung--" + i);
+                Thread.Sleep(100);
             }
         }
     }
