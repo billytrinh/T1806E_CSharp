6261af2a6a55891f53e2133a3ac035954225100e
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-05-23 18:57:46 +0700

    thread

--- T1806E_CSharp/T1806E_CSharp/Session9/ThreadDemo.cs
+++ T1806E_CSharp/T1806E_CSharp/Session9/ThreadDemo.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Threading;
 namespace T1806E_CSharp.Session9
 {
+    //public delegate void running(Object o);
+
     public class ThreadDemo
     {
-        public ThreadDemo()
+        public static void Main(string[] args)
         {
+            RunOnThread("Main");
+            Thread t = new Thread(RunOnThread);
+            t.Start("Thread 2");
+        }
+
+        public static void RunOnThread(Object a)
+        {
+            String s = (String)a;   
+            for(int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(s +"--"+ i);
+                Thread.Sleep(300);
+            }
         }
     }
 }
